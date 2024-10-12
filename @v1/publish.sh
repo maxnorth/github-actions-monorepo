92f6d735fc1055257de77a40c40d81ebf3a1f409
Max North <maxwelldnorth@gmail.com>
2024-10-12 09:29:26 -0400

    Fixup

--- @v1/publish.sh
+++ @v1/publish.sh
@@ -13,8 +13,9 @@ for dest_branch in $dest_branches; do
     # clear existing contents and create new content
     git rm -rf -q ./*
     git checkout -q $src_branch .
-    find . -type d -name "@*" | xargs git rm -rf
-    git checkout -q $src_branch "*/@$dest_branch/*"
+
+    # remove @folders unrelated to this branch
+    find . -type d -name "@*" | grep -vE "/@$dest_branch$" | xargs git rm -rf
 
     # hoist @folder contents up one level
     for dest_branch_path in $(find . -type d -name "@*"); do
