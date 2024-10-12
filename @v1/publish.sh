97242194d4b65fce8b64e89151a7bf5d01cc26f7
Max North <maxwelldnorth@gmail.com>
2024-10-12 09:46:37 -0400

    Fixup

--- @v1/publish.sh
+++ @v1/publish.sh
@@ -8,27 +8,27 @@ echo "Preparing to publish to branches: $dest_branches"
 
 for dest_branch in $dest_branches; do
     # switch to branch being deployed
-    git checkout $dest_branch || git checkout -b $dest_branch 1> .log.verbose
+    git checkout $dest_branch || git checkout -b $dest_branch > .log.verbose
 
     # clear existing contents and create new content
-    git rm -rf ./* 1> .log.verbose
-    git checkout $src_branch . 1> .log.verbose
+    git rm -rf ./* > .log.verbose
+    git checkout $src_branch . > .log.verbose
 
     # remove @folders unrelated to this branch
-    find . -type d -name "@*" | grep -vE "/@$dest_branch$" | xargs git rm -rf 1> .log.verbose
+    find . -type d -name "@*" | grep -vE "/@$dest_branch$" | xargs git rm -rf > .log.verbose
 
     # hoist @folder contents up one level
     for dest_branch_path in $(find . -type d -name "@*"); do
-        mv ./$dest_branch_path/* ./$dest_branch_path/../ 1> .log.verbose
+        mv ./$dest_branch_path/* ./$dest_branch_path/../ > .log.verbose
     done
 
     # remove old @folders
-    find . -type d -name "@*" | xargs rm -rf 1> .log.verbose
+    find . -type d -name "@*" | xargs rm -rf > .log.verbose
 
     # commit and push
     git add -A 1> .log.verbose
-    git commit -m $current_commit_msg 1> .log.verbose
-    git push -uf origin HEAD 1> .log.verbose
+    git commit -m $current_commit_msg > .log.verbose
+    git push -u origin HEAD > .log.verbose
 done
 
 git checkout $src_branch
