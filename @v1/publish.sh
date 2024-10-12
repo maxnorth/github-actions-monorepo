a2b0108e385d8bc2bab94ce45e693d74542d24d6
Max North <maxwelldnorth@gmail.com>
2024-10-12 09:17:58 -0400

    Fixup

--- @v1/publish.sh
+++ @v1/publish.sh
@@ -1,22 +1,20 @@
 #! /bin/bash
 
-shopt -s globstar
-
 src_branch=$(git rev-parse --abbrev-ref HEAD)
 current_commit_msg=$(git log -1 --pretty=%B)
 dest_branches=$(find . -type d -name "@*" | xargs basename | cut -c2- | sort | uniq)
 
-echo "Preparing to publish to branches: $(echo $dest_branches | tr '\n' ',')"
+echo "Preparing to publish to branches: $dest_branches"
 
 for dest_branch in $dest_branches; do
     # switch to branch being deployed
     git checkout $dest_branch || git checkout -b $dest_branch
 
     # clear existing contents and create new content
-    git rm -rf ./*
-    git checkout $src_branch .
+    git rm -rf -q ./*
+    git checkout -q $src_branch .
     find . -type d -name "@*" | xargs git rm -rf
-    git checkout $src_branch "*/@$dest_branch/*"
+    git checkout -q $src_branch "*/@$dest_branch/*"
 
     # hoist @folder contents up one level
     for dest_branch_path in $(find . -type d -name "@*"); do
