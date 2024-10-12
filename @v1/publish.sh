fa0a75f0e3835b0fde988e5e31ccc077786ec762
Max North <maxwelldnorth@gmail.com>
2024-10-11 23:09:32 -0400

    Add workflow stubs

---
+++ @v1/publish.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+
+
+git checkout $DEST_BRANCH || git checkout -b $DEST_BRANCH
+git rm -rf \*
+git checkout $SRC_BRANCH .
+git rm -rf \*_/@_/
+git checkout $SRC_BRANCH "*/@$DEST_BRANCH/\*"
+for $(ls \*_/@_/)- mv ./$FILE_PATH/* ./$FILE_PATH/../
+rm -rf \*\*/@$DEST_BRANCH/
+git add -A
+git commit -m "publish bla"
