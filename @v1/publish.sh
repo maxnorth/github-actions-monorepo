d7a5bd6550b37a1643f44c677e66eff54e902a54
Max North <maxwelldnorth@gmail.com>
2024-10-12 08:36:42 -0400

    Add publish script

--- @v1/publish.sh
+++ @v1/publish.sh
@@ -1,13 +1,28 @@
 #! /bin/bash
 
+current_commit_msg=$(git log -1 --pretty=%B)
+dest_branches=$(ls -d ./**/@*/ | xargs basename | cut -c2- | sort | uniq)
 
+for dest_branch in $dest_branches; do
+    # switch to branch being deployed
+    git checkout $dest_branch || git checkout -b $dest_branch
 
-git checkout $DEST_BRANCH || git checkout -b $DEST_BRANCH
-git rm -rf \*
-git checkout $SRC_BRANCH .
-git rm -rf \*_/@_/
-git checkout $SRC_BRANCH "*/@$DEST_BRANCH/\*"
-for $(ls \*_/@_/)- mv ./$FILE_PATH/* ./$FILE_PATH/../
-rm -rf \*\*/@$DEST_BRANCH/
-git add -A
-git commit -m "publish bla"
+    # clear existing contents and create new content
+    git rm -rf ./*
+    git checkout $src_branch .
+    git rm -rf ./**/@*/
+    git checkout $src_branch "*/@$dest_branch/*"
+
+    # hoist @folder contents up one level
+    for dest_branch_path in ls ./**/@*/; do
+        mv ./$dest_branch_path/* ./$dest_branch_path/../
+    done
+
+    # remove old @folders
+    rm -rf \*\*/@$dest_branch/
+
+    # commit and push
+    git add -A
+    git commit -m $current_commit_msg
+    git push -u origin HEAD
+done
